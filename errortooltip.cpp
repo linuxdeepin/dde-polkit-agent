089acc483e103d3b6ddd2d46fe3966b08a2dc207
Hualet Wang <mr.asianwang@gmail.com>
2016-11-23 13:05:59 +0800

    Fix tooltip window misbehavior
    SplashWindow type window will leave remaining image after its deletion.
    
    Change-Id: I9206a3f60a80e0224c768a6c49b70a1078870607

--- errortooltip.cpp
+++ errortooltip.cpp
@@ -8,6 +8,8 @@ ErrorTooltip::ErrorTooltip(QString errorMessage) :
     DArrowRectangle(DArrowRectangle::ArrowTop),
     m_label(new QLabel(errorMessage))
 {
+    setWindowFlags(Qt::ToolTip);
+
     m_label->setStyleSheet("padding: 5px 10px; color: #f9704f");
     m_label->adjustSize();
     setContent(m_label);
