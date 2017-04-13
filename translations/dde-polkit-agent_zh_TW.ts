12894bce8ce87a0ac5cd8400c2adcbdb2f558d6b
freddy4212 <freddy4212@gmail.com>
2017-04-13 04:58:08 -0500

    Correct the zh_TW translation.
    Correct the zh_TW translation.

---
+++ translations/dde-polkit-agent_zh_TW.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>AuthDialog</name>
+    <message>
+        <location filename="../AuthDialog.cpp" line="26"/>
+        <source>Wrong password</source>
+        <translation>密碼錯誤</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="186"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="186"/>
+        <source>Confirm</source>
+        <translation>驗證</translation>
+    </message>
+</context>
+</TS>
