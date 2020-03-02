bb60c8b514a839c8b07dc60bf60b52c9c09c2536
fanpengcheng_cm <fanpengcheng_cm@deepin.com>
2020-03-02 22:29:27 +0800

    feat:update ts
    更新翻译文案，并修改三次密码错误后显示错误提示3s后再关闭窗口.(task:15770)

--- translations/dde-polkit-agent.ts
+++ translations/dde-polkit-agent.ts
@@ -1,28 +1,43 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>AuthDialog</name>
     <message>
-        <location filename="../AuthDialog.cpp" line="131"/>
+        <location filename="../AuthDialog.cpp" line="120"/>
         <source>Verify your fingerprint or password</source>
-        <translation type="unfinished"></translation>
+        <translation>请验证指纹或输入密码</translation>
     </message>
     <message>
-        <location filename="../AuthDialog.cpp" line="269"/>
+        <location filename="../AuthDialog.cpp" line="231"/>
+        <source>You are required to change your password immediately (password expired)</source>
+        <translation>您的密码已过期，请更改</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="287"/>
+        <source>Wrong password, only one chance left</source>
+        <translation>密码错误，只剩1次机会</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="290"/>
+        <source>Wrong password, two chances left</source>
+        <translation>密码错误，还有2次机会</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="294"/>
         <source>Wrong password</source>
-        <translation type="unfinished"></translation>
+        <translation>密码错误</translation>
     </message>
     <message>
-        <location filename="../AuthDialog.cpp" line="278"/>
+        <location filename="../AuthDialog.cpp" line="306"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
     <message>
-        <location filename="../AuthDialog.cpp" line="148"/>
-        <location filename="../AuthDialog.cpp" line="279"/>
+        <location filename="../AuthDialog.cpp" line="137"/>
+        <location filename="../AuthDialog.cpp" line="307"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>认证</translation>
     </message>
 </context>
 </TS>
