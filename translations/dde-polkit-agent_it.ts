3081c694c3d0b4c5ff55aecadabffda8283be851
songwentai <songwentai@deepin.com>
2020-03-03 11:00:57 +0000

    chore: auto pull translation files from transifex

--- translations/dde-polkit-agent_it.ts
+++ translations/dde-polkit-agent_it.ts
@@ -2,25 +2,40 @@
 <context>
     <name>AuthDialog</name>
     <message>
-        <location filename="../AuthDialog.cpp" line="131"/>
+        <location filename="../AuthDialog.cpp" line="123"/>
         <source>Verify your fingerprint or password</source>
         <translation>Verifica l&apos;impronta o la password</translation>
     </message>
     <message>
-        <location filename="../AuthDialog.cpp" line="269"/>
+        <location filename="../AuthDialog.cpp" line="140"/>
+        <location filename="../AuthDialog.cpp" line="327"/>
+        <source>Confirm</source>
+        <translation>Conferma</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="239"/>
+        <source>You are required to change your password immediately (password expired)</source>
+        <translation>Occorre cambiare immediatamente la password (password scaduta)</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="307"/>
+        <source>Wrong password, only one chance left</source>
+        <translation>Password errata, ultimo tentativo disponibile</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="310"/>
+        <source>Wrong password, two chances left</source>
+        <translation>Password errata, due tentativi ancora disponibili</translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="314"/>
         <source>Wrong password</source>
         <translation>Password errata</translation>
     </message>
     <message>
-        <location filename="../AuthDialog.cpp" line="278"/>
+        <location filename="../AuthDialog.cpp" line="326"/>
         <source>Cancel</source>
         <translation>Annulla</translation>
-    </message>
-    <message>
-        <location filename="../AuthDialog.cpp" line="148"/>
-        <location filename="../AuthDialog.cpp" line="279"/>
-        <source>Confirm</source>
-        <translation>Conferma</translation>
     </message>
 </context>
 </TS>
