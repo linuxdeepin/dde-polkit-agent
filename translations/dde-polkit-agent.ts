39183ceae85b0122b26d1747a716d45713c2ed87
fanpengcheng_cm <fanpengcheng_cm@deepin.com>
2020-03-03 18:23:22 +0800

    feat:update ts
    更新翻译文案，并修改三次密码错误后显示错误提示3s后再关闭窗口.(task:15770)

--- translations/dde-polkit-agent.ts
+++ translations/dde-polkit-agent.ts
@@ -1,43 +1,43 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.1">
 <context>
     <name>AuthDialog</name>
     <message>
-        <location filename="../AuthDialog.cpp" line="120"/>
+        <location filename="../AuthDialog.cpp" line="123"/>
         <source>Verify your fingerprint or password</source>
-        <translation>请验证指纹或输入密码</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../AuthDialog.cpp" line="231"/>
-        <source>You are required to change your password immediately (password expired)</source>
-        <translation>您的密码已过期，请更改</translation>
-    </message>
-    <message>
-        <location filename="../AuthDialog.cpp" line="287"/>
-        <source>Wrong password, only one chance left</source>
-        <translation>密码错误，只剩1次机会</translation>
-    </message>
-    <message>
-        <location filename="../AuthDialog.cpp" line="290"/>
-        <source>Wrong password, two chances left</source>
-        <translation>密码错误，还有2次机会</translation>
-    </message>
-    <message>
-        <location filename="../AuthDialog.cpp" line="294"/>
-        <source>Wrong password</source>
-        <translation>密码错误</translation>
-    </message>
-    <message>
-        <location filename="../AuthDialog.cpp" line="306"/>
-        <source>Cancel</source>
-        <translation>取消</translation>
-    </message>
-    <message>
-        <location filename="../AuthDialog.cpp" line="137"/>
-        <location filename="../AuthDialog.cpp" line="307"/>
+        <location filename="../AuthDialog.cpp" line="140"/>
+        <location filename="../AuthDialog.cpp" line="327"/>
         <source>Confirm</source>
-        <translation>认证</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="239"/>
+        <source>You are required to change your password immediately (password expired)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="307"/>
+        <source>Wrong password, only one chance left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="310"/>
+        <source>Wrong password, two chances left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="314"/>
+        <source>Wrong password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AuthDialog.cpp" line="326"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
