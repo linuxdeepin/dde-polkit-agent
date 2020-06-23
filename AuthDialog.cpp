75603009b7162259ce2396f04f11a59336e53fc0
fengshaoxiong <fengshaoxiong@uniontech.com>
2020-06-23 17:26:07 +0800

    fix: 解决标题内容过多的时候，导致用户框显示问题
    标题内容过多的时候对话框的宽度增大，而用户框使用的是固定宽度，将用户框的宽度改成自适应
    
    Log: 修改用户框显示问题
    Bug: https://pms.uniontech.com/zentao/bug-view-34016.html

--- AuthDialog.cpp
+++ AuthDialog.cpp
@@ -344,7 +344,7 @@ void AuthDialog::setupUI()
     icon.setDevicePixelRatio(dpr);
     setIcon(icon);
 
-    m_adminsCombo->setMaximumWidth(maximumWidth());
+    m_adminsCombo->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     m_adminsCombo->hide();
     m_passwordInput->setEchoMode(QLineEdit::Password);
     m_tooltip->hide();
