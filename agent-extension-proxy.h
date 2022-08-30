60f37f9e7f7ec53e666a9d2115dbed8a79a25a19
shaojun <wangshaojun@uniontech.com>
2022-08-30 16:59:37 +0800

    chore: adapt license and copyright
    Modify project to pass the REUSE check
    
    Log: Modify project to pass the REUSE check
    Task: https://pms.uniontech.com/task-view-185215.html
    Change-Id: I4158bd15d18049c312f09a3be923f618d8ccfddb

--- agent-extension-proxy.h
+++ agent-extension-proxy.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef AGENTEXTENSIONPROXY_H
 #define AGENTEXTENSIONPROXY_H
 #include <QString>
--- tests/polkit.cc
+++ tests/polkit.cc
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <QString>
 #include <polkit-qt5-1/PolkitQt1/Agent/Listener>
 #include <polkit-qt5-1/PolkitQt1/Agent/Session>
